45b5f4be818b9faccaa4a319573dbd320d4c7ef1
Yandisa <gaju.yandisa@gmail.com>
2026-06-07 15:23:21 +0200

    Update settings.py

--- yummybakes/settings.py
+++ yummybakes/settings.py
@@ -112,8 +112,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'xolagaju8@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'eerbkpzsvkcseexq')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = 'Yummy Bakes <xolagaju8@gmail.com>'
 
 # Production security (only when DEBUG=False AND on HTTPS)
